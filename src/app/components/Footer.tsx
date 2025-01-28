e927a070e5768cf97d9e42fde4a3d022fd5d0650
Axel Velasquez <yoloax11@gmail.com>
2025-01-27 20:56:44 -0800

    updated the footer to include icons

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { BsInstagram, BsFacebook, BsDiscord } from "react-icons/bs";
+import Link from "next/link";
 
 const Footer = () => {
 
@@ -14,6 +16,17 @@ const Footer = () => {
 
     return (
         <footer className="bg-grat-100 py-8 px-4 sm:px-6 lg:px-8">
+            <div className="flex flex-row wrap gap-20 justify-center mb-10">
+                <Link href="https://www.instagram.com/leclubdefrancaislangara/">
+                    <BsInstagram className="hover:-translate-y-1 duration-300 size-10 text-pink-600" />
+                </Link>
+                <Link href="https://www.facebook.com/langara.french.club/">
+                    <BsFacebook className="size-10 text-blue-600 hover:-translate-y-1 duration-300" />
+                </Link>
+                <Link href="https://discord.gg/9UQchagN43">
+                    <BsDiscord className="size-10 text-blue-900 hover:-translate-y-1 duration-300" />
+                </Link>
+            </div>
             <div className="max-w-7xl mx-auto text-center">
                 <p className="text-black">&copy; {year} Club Français de Langara. Tous droits réservés.</p>
             </div>
