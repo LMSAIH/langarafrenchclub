82f3a62c8241e5c093a45c510e2b9b3653ea8522
Axel Velasquez <yoloax11@gmail.com>
2025-01-29 22:37:59 -0800

    added the member content

---
+++ src/app/PageContent/MembersContent.ts
@@ -0,0 +1,108 @@
+const MembersContent = {
+  en: {
+    currentExecsTitle: "Current Club Leaders",
+    oldExecsTitle: "Former Club Leaders",
+    teamIntro: "Here is our amazing team!",
+    close: "Close",
+    currentExecs: [
+      {
+        name: "Aurora Miller",
+        role: "Vice-President",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Amar Miller",
+        role: "President",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Joseph Miller",
+        role: "Director of events",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Joseph Miller",
+        role: "Director of events",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Joseph Miller",
+        role: "Director of events",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Joseph Miller",
+        role: "Director of events",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+    ],
+    oldExecs: [
+      {
+        name: "Aurora Miller",
+        role: "Vice-President",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+    ],
+  },
+  fr: {
+    close: "Fermer",
+    currentExecsTitle: "Les dirigeants actuels du club",
+    oldExecsTitle: "Les anciens dirigeants du club",
+    teamIntro: "Voici notre incroyable équipe !",
+    currentExecs: [
+      {
+        name: "Aurora Miller",
+        role: "Vice-Président",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Amar Miller",
+        role: "Président",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Joseph Miller",
+        role: "Directeur des événements",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Joseph Miller",
+        role: "Directeur des événements",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Joseph Miller",
+        role: "Directeur des événements",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+      {
+        name: "Joseph Miller",
+        role: "Directeur des événements",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+    ],
+    oldExecs: [
+      {
+        name: "Aurora Miller",
+        role: "Vice-Président",
+        bio: "Blablabla",
+        imageUrl: "/CONSAR1.jpg",
+      },
+    ],
+  },
+};
+
+export default MembersContent;
