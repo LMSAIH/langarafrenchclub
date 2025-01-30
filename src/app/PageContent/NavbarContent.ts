33ea3687cd64a296a3f74267c9151fb3e61df296
Axel Velasquez <yoloax11@gmail.com>
2025-01-29 22:38:07 -0800

    added the navbar content

---
+++ src/app/PageContent/NavbarContent.ts
@@ -0,0 +1,20 @@
+const NavbarContent = {
+    en: {
+      home: "Home",
+      members: "Members",
+      events: "Events",
+      sponsors: "Sponsors",
+      contact: "Contact",
+      language: "EN",
+    },
+    fr: {
+      home: "Accueil",
+      members: "Membres",
+      events: "Événements",
+      sponsors: "Sponsors",
+      contact: "Contact",
+      language: "FR",
+    },
+  };
+  
+  export default NavbarContent;
