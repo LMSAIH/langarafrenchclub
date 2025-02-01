f225ba1a172400eb01813cc067278c483dba4ba2
Axel Velasquez <yoloax11@gmail.com>
2025-01-31 18:09:35 -0800

    responsive to big screens

--- src/app/components/SponsorModal.tsx
+++ src/app/components/SponsorModal.tsx
@@ -45,7 +45,7 @@ const SponsorModal: React.FC<SponsorModalProps> = ({ sponsor, isOpen, onClose })
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+            <div className="inline-block w-full max-w-md 4xl:max-w-4xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
               <div className="relative h-48 mb-4">
                 <Image
                   src={sponsor.logo}
@@ -54,20 +54,20 @@ const SponsorModal: React.FC<SponsorModalProps> = ({ sponsor, isOpen, onClose })
                   objectFit="contain"
                 />
               </div>
-              <DialogPanel as="h3" className="text-2xl font-medium leading-6 text-gray-900 mb-2">
+              <DialogPanel as="h3" className="text-2xl 4xl:text-8xl 4xl:mb-10 font-medium leading-6 text-gray-900 mb-2">
                 {sponsor.name}
               </DialogPanel>
-              <p className="text-gray-700 mb-4">{sponsor.description}</p>
+              <p className="text-gray-700 mb-4 4xl:text-5xl 4xl:mb-10">{sponsor.description}</p>
               <a
                 href={sponsor.website}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="inline-block bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300"
+                className="inline-block bg-blue-600 4xl:text-5xl 4xl:mb-10 4xl:p-4 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300"
               >
                 Visiter le site web
               </a>
-              <button onClick={onClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
-                <X size={24} />
+              <button onClick={onClose} className="absolute top-2 right-2  text-gray-500 hover:text-gray-700">
+                <X size={24} className="4xl:size-12" />
               </button>
             </div>
           </TransitionChild>
