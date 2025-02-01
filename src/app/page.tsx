cf9f13ad8e879fabe2bfcaec25f124bc04b47b37
Axel Velasquez <yoloax11@gmail.com>
2025-01-31 18:08:48 -0800

    modified the page to acccount for big monitors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
         </div>
         <div className="relative z-10 flex items-center justify-center h-full">
           <h1
-            className="text-4xl md:text-6xl text-white font-bold text-center px-4"
+            className="text-4xl md:text-6xl 4xl:text-8xl text-white font-bold text-center px-4"
             data-aos="fade-up"
             data-aos-delay="50"
           >
@@ -55,26 +55,26 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-white py-16 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto">
+      <div className="bg-white py-16 px-4 sm:px-6 lg:px-8 w-full">
+        <div className="max-w-7xl 4xl:max-w-full 4xl:w-3/4 mx-auto">
           <div
-            className="flex flex-col md:flex-row items-center justify-between mb-12"
+            className="flex flex-col md:flex-row items-center justify-between mb-12 4xl:w-full"
             data-aos="fade-up-right"
             data-aos-delay="50"
           >
-            <h2 className="text-3xl font-bold text-white mb-4 md:mb-0 bg-blue-900 p-2 rounded-md md:p-6">
+            <h2 className="text-3xl 4xl:text-8xl 4xl:p-12 font-bold text-white mb-4 md:mb-0 bg-blue-900 p-2 rounded-md md:p-6 4xl:rounded-xl" >
               {content.aboutTitle}
             </h2>
-            <Image src="/Question_image.svg" alt="question image" width={150} height={150} className="w-1/2" />
+            <Image src="/Question_image.svg" alt="question image" width={150} height={150} className="w-1/2 4xl:w-3/4" />
           </div>
 
-          <div className="flex flex-col md:flex-row items-center justify-between mt-20" data-aos="fade-up-left">
-            <Image src="/Answer_image.svg" alt="answer image" width={100} height={100} className="mb-4 md:mb-0 md:relative md:w-1/2 absolute md:block hidden  top-1/4 w-full" />
-            <p className="text-xl md:text-2xl text-white bg-red-600 md:bg-opacity-100 md:ml-8 p-4 rounded-md md:rounded-md md:p-6 md:bg-red-600  "
+          <div className="flex flex-col md:flex-row items-center justify-between mt-20 4xl:w-full" data-aos="fade-up-left" >
+            <Image src="/Answer_image.svg" alt="answer image" width={100} height={100} className="mb-4 md:mb-0 md:relative md:w-1/2 md:block hidden 4xl:w-3/4 " />
+            <p className="text-xl md:text-2xl 4xl:text-6xl 4xl:p-12 4xl:w-full 4xl:rounded-xl text-white bg-red-600 md:bg-opacity-100 md:ml-8 p-4 rounded-md md:rounded-md md:p-6 md:bg-red-600  "
               data-aos="zoom-y-out" data-aos-delay="100">
               {content.aboutText}
             </p>
-            <Image src="/Answer_image.svg" alt="answer image" width={100} height={100} className="mb-4 mt-10 md:mb-0 md:relative md:w-1/2 relative md:hidden block  w-full" />
+            <Image src="/Answer_image.svg" alt="answer image" width={100} height={100} className="mb-4 mt-10 md:mb-0 md:relative md:w-1/2 relative md:hidden block w-full" />
           </div>
         </div>
       </div>
@@ -94,11 +94,11 @@ export default function Home() {
       </div>
 
       <div className="bg-red-700 py-16 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto">
-          <h2 className="text-3xl font-bold text-white mb-8 text-center" data-aos="fade-up">
+        <div className="max-w-7xl 4xl:max-w-full 4xl:w-3/4 mx-auto">
+          <h2 className="text-3xl font-bold text-white mb-8 text-center 4xl:text-8xl 4xl:mb-16" data-aos="fade-up">
             {content.eventsTitle}
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="flex flex-row flex-wrap gap-10 justify-center">
             {content.events.map((event, index) => (
               <EventCard key={`event ${index}`} title={event.title} description={event.description} date={event.date} />
             ))}
@@ -121,11 +121,11 @@ export default function Home() {
 
 
       <div className="bg-white py-16 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto">
-          <h2 className="text-3xl font-bold text-blue-900 mb-8 text-center" data-aos="fade-up">
+        <div className="max-w-7xl 4xl:max-w-full 4xl:w-3/4 mx-auto">
+          <h2 className="text-3xl 4xl:text-8xl font-bold text-blue-900 mb-8 text-center" data-aos="fade-up">
             {content.testimonialsTitle}
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="flex flex-row wrap gap-10 justify-center">
             {content.testimonials.map((testimonial, index) => (
               <TestimonialCard key={`testimonial ${index}`} quote={testimonial.quote} author={testimonial.author} />
             ))}
@@ -147,16 +147,16 @@ export default function Home() {
       </div>
 
       <div className="bg-blue-900 py-16 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto text-center">
-          <h2 className="text-3xl font-bold text-white mb-4" data-aos="fade-up">
+        <div className="max-w-7xl 4xl:max-w-full 4xl:w-3/4 mx-auto text-center">
+          <h2 className="text-3xl 4xl:text-8xl font-bold text-white mb-4 4xl:mb-20" data-aos="fade-up">
             {content.contactTitle}
           </h2>
-          <p className="text-xl text-white mb-8" data-aos="fade-up" data-aos-delay="100">
+          <p className="text-xl 4xl:text-6xl text-white mb-8 4xl:mb-20 4xl:mt-20 " data-aos="fade-up" data-aos-delay="100">
             {content.contactText}
           </p>
           <a
             href="mailto:frenchclub@langara.ca"
-            className="inline-block bg-white text-blue-900 font-bold py-3 px-8 rounded-full hover:bg-red-700 hover:text-white transition duration-300"
+            className="inline-block bg-white text-blue-900 4xl:text-6xl 4xl:p-12 font-bold py-3 px-8 rounded-full hover:bg-red-700 hover:text-white transition duration-300"
             data-aos="zoom-in"
           >
             {content.contactButton}
