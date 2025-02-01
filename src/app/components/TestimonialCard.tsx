4dc6b2273643461873935d21d089f3a7d1b4c008
Axel Velasquez <yoloax11@gmail.com>
2025-01-31 18:09:50 -0800

    modified the testimonial card to account for big screens

--- src/app/components/TestimonialCard.tsx
+++ src/app/components/TestimonialCard.tsx
@@ -9,9 +9,9 @@ interface TestimonialCardProps {
 
 const TestimonialCard: React.FC<TestimonialCardProps> = ({ quote, author }) => {
   return (
-    <div className="bg-gray-100 rounded-lg p-6" data-aos="fade-up">
-      <p className="text-gray-700 italic mb-4">"{quote}"</p>
-      <p className="text-blue-900 font-semibold">- {author}</p>
+    <div className="bg-gray-100 rounded-lg p-6 4xl:p-12 4xl:rounded-xl" data-aos="fade-up">
+      <p className="text-gray-700 italic mb-4 4xl:text-5xl xl:mt-10">"{quote}"</p>
+      <p className="text-blue-900 font-semibold 4xl:text-4xl 4xl:mt-10">- {author}</p>
     </div>
   )
 }
