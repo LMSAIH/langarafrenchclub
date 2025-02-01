48084b40df2969ba95a9f1cd6984fbf0bb328406
Axel Velasquez <yoloax11@gmail.com>
2025-01-31 18:08:58 -0800

    modified the page to acccount for big monitors

--- src/app/components/EventCard.tsx
+++ src/app/components/EventCard.tsx
@@ -10,10 +10,10 @@ interface EventCardProps {
 
 const EventCard: React.FC<EventCardProps> = ({ title, date, description }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6" data-aos="fade-up">
-      <h3 className="text-xl font-bold text-blue-900 mb-2">{title}</h3>
-      <p className="text-red-700 font-semibold mb-2">{date}</p>
-      <p className="text-gray-600">{description}</p>
+    <div className="bg-white rounded-lg shadow-md p-6 4xl:p-12 4xl:rounded-xl" data-aos="fade-up">
+      <h3 className="text-xl font-bold text-blue-900 mb-2 4xl:text-5xl">{title}</h3>
+      <p className="text-red-700 font-semibold mb-2 4xl:text-4xl">{date}</p>
+      <p className="text-gray-600 4xl:text-4xl">{description}</p>
     </div>
   )
 }
